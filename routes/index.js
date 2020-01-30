d53629290f6502a540542b4a8976db7a17b4155e
Daffy Chu <danielchu0710@hotmail.com>
2020-01-30 09:33:28 -0500

    [AR-3] Update with more CRUD api calls and to sync up

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,24 @@ router.get('/', function(req, res, next) {
 router.get('/api', function(req, res, next) {
   res.render('index', { title: 'Arezue API' });
 });
+
 // Temporarily get request for testing
 router.get('/api/Users', db.getAllUsers);
 
-// C of CRUD, create account for new jobseeker
+// Create account for new jobseeker
 router.post('/api/CreateAccount/jobseeker/', db.createJobseeker);
 
 // Create account for new employer
 router.post('/api/CreateAccount/employer/', db.createEmployer);
 
-// Add a field
-// router.post('/api/Jobseeker/addPhone', db.addJobseeker);
+// Add a phone number to jobseeker's row
+router.post('/api/Jobseeker/addPhoneNum', db.updateJobseekerPhoneNum);
+
+// Add a phone number to employer's row
+router.post('/api/Employer/addPhoneNum', db.updateEmployerPhoneNum);
+
+// Not being used method, DO NOT USE
+router.post('/api/addPhoneNum', db.addPhoneNum);
 
 module.exports = router;
+ 
--- routes/queries.js
+++ routes/queries.js
@@ -4,8 +4,13 @@ const pool = require('../config');
 var express = require('express')
 var bodyParser = require('body-parser')
 
-const { body,validationResult } = require('express-validator');
-const { sanitizeBody } = require('express-validator');
+const {
+    body,
+    validationResult
+} = require('express-validator');
+const {
+    sanitizeBody
+} = require('express-validator');
 
 var app = express()
 
@@ -13,7 +18,9 @@ var app = express()
 var jsonParser = bodyParser.json()
 
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
+var urlencodedParser = bodyParser.urlencoded({
+    extended: true
+})
 
 // we can also do exports.func
 exports.getAllUsers = function (request, response, next) {
@@ -28,66 +35,204 @@ exports.getAllUsers = function (request, response, next) {
 // Route to create the jobseeker account
 // Since they have different views, we will have differnet POST api request
 /*
-** Requirements: UUID, Email and Name
-*/
+ ** Requirements: UUID, Email and Name
+ */
 exports.createJobseeker = [
     body('email').notEmpty().isEmail().withMessage("Must be a valid email address"),
     body('name').notEmpty().isAlpha().withMessage("Must be a valid name"),
     sanitizeBody('name', 'email').escape(),
 
-    async function(req, res, next) {
+    async function (req, res, next) {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             // There are errors. Render form again with sanitized values/errors messages.
             res.status(400).send(errors)
             return;
         }
-        else {
-            let firebaseID = req.body.firebaseID;
-            let name = req.body.name;
-            let email = req.body.email;
-            pool.query('INSERT INTO jobseeker (uid, name, email_address) VALUES (uuid_generate_v4(), $1, $2) RETURNING uid', [name, email], (error, results) => {
-                if (error) {
-                    res.status(400).send(error)
-                    return console.error(error);
-                }
-                res.status(200).send(`User created with ID: ${results.rows[0].uid}`)
-            })
-        }
-}];
+        let firebaseID = req.body.firebaseID;
+        let name = req.body.name;
+        let email = req.body.email;
+        pool.query('INSERT INTO jobseeker (uid, name, email_address) VALUES (uuid_generate_v4(), $1, $2) RETURNING uid', [name, email], (error, results) => {
+            if (error) {
+                res.status(400).send(error)
+                return console.error(error);
+            }
+            res.status(200).send(`User created with ID: ${results.rows[0].uid}`)
+        })
+    }
+];
 
+
+// Route to create the Employer account
+// Since they have different views, we will have differnet POST api request
+/*
+ ** Requirements: UUID, Email and Name
+ */
 exports.createEmployer = [
     body('email').notEmpty().isEmail().withMessage("Must be a valid email address"),
     body('name').notEmpty().isAlpha().withMessage("Must be a valid name"),
     body('company').notEmpty().withMessage("Must be a valid company name"),
     sanitizeBody('name', 'company', 'email').escape(),
 
-    async function(req, res, next) {
+    async function (req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).send(errors)
+            return;
+        }
+        let firebaseID = req.body.firebaseID;
+        let name = req.body.name;
+        let email = req.body.email;
+        let company = req.body.company;
+        pool.query('INSERT INTO employer (uid, name, email_address, company VALUES (uuid_generate_v4(), $1, $2, $3) RETURNING uid', [name, email, company], (error, results) => {
+            if (error) {
+                res.status(400).send(error)
+                return console.error(error);
+            }
+            res.status(200).send(`User created with ID: ${results.rows[0].uid}`)
+        })
+
+
+    }
+];
+
+// This function will not be used as we decided to use different route for employer and jobseeker on updating their information
+exports.addPhoneNum = [
+    body('phone').notEmpty().isMobilePhone().withMessage("Must be a valid phone number"),
+    body('uuid').notEmpty().withMessage("Must provide a valid UUID"),
+    sanitizeBody('phone', 'uuid').escape(),
+
+    async function (req, res, next) {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             res.status(400).send(errors)
             return;
         } else {
-            let firebaseID = req.body.firebaseID;
-            let name = req.body.name;
-            let email = req.body.email;
-            let company = req.body.company;
-            pool.query('INSERT INTO employer (uid, name, email_address, company VALUES (uuid_generate_v4(), $1, $2, $3) RETURNING uid', [name, email, company], (error, results) => {
+            let firebaseID = req.body.firebaseID; // Unsure if needed at this stage right now
+            let phone = req.body.phone;
+            let uuid = req.body.uuid;
+            await pool.query(`select email_address from jobseeker
+                WHERE uid = $1`, [uuid], (error, results) => {
                 if (error) {
                     res.status(400).send(error)
                     return console.error(error);
                 }
-                res.status(200).send(`User created with ID: ${results.rows[0].uid}`)
-            })
+                if (results.rowCount) {
+                    pool.query(`UPDATE jobseeker
+                    SET phone_number = $1
+                    WHERE
+                        uid = $2`, [phone, uuid], (error, results) => {
+                        if (error) {
+                            res.status(400).send(error)
+                            return console.error(error);
+                        }
+                        res.status(200).send('INSERTED!');
+                    });
+                } else {
+                    res.status(404).send("MISSING DATA!");
+                }
+            });
         }
+    }
+];
+
+// Method to insert phoneNumber to a specific jobseeker
+exports.updateJobseekerPhoneNum = [
+    body('phone').notEmpty().isMobilePhone().withMessage("Must be a valid phone number"),
+    body('uuid').notEmpty().withMessage("Must provide a valid UUID"),
+    sanitizeBody('phone', 'uuid').escape(),
+
+    async function (req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).send(errors)
+            return;
+        }
+        let phone = req.body.phone;
+        let uuid = req.body.uuid;
+        await pool.query(`Update jobseeker
+        SET phone_number = $1
+        WHERE
+            uid = $2
+        RETURNING uid`, [phone, uuid], (error, results) => {
+            if (error) {
+                res.status(400).send(error)
+                return console.error(error);
+            }
+            if (results.rowCount) {
+                res.status(200).send("Phone number inserted");
+            } else {
+                res.status(400).send("User not found");
+            }
+
+        });
 
     }
 ];
 
-// exports.addJobseeker = [
+// Method to insert phoneNumber to a specific employer
+exports.updateEmployerPhoneNum = [
+    body('phone').notEmpty().isMobilePhone().withMessage("Must be a valid phone number"),
+    body('uuid').notEmpty().withMessage("Must provide a valid UUID"),
+    sanitizeBody('phone', 'uuid').escape(),
 
-// ]
+    async function (req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).send(errors)
+            return;
+        }
+        let phone = req.body.phone;
+        let uuid = req.body.uuid;
+        await pool.query(`Update employer
+        SET phone_number = $1
+        WHERE
+            uid = $2
+        RETURNING uid`, [phone, uuid], (error, results) => {
+            if (error) {
+                res.status(400).send(error)
+                return console.error(error);
+            }
+            if (results.rowCount) {
+                res.status(200).send("Phone number inserted");
+            } else {
+                res.status(400).send("User not found");
+            }
 
+        });
+
+    }
+];
+
+exports.updateJobseekerPotential = [
+    body('potential').notEmpty().isBoolean().withMessage("Must be a boolean value"),
+    body('uuid').notEmpty().withMessage("Must provide a valid UUID"),
+    sanitizeBody('potential', 'uuid').escape(),
+
+    async function(req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).send(errors)
+            return;
+        }
+        let potential = req.body.potential;
+        await pool.query(`Update jobseeker
+        SET potential_client = $1
+        WHERE
+            uid = $2
+        RETURNING uid`, [potential, uid], (error, results) => {
+            if (error) {
+                res.status(400).send(error)
+                return console.error(error);
+            }
+            if (results.rowCount) {
+                res.status(200).send("Potential Client modified");
+            } else {
+                res.status(400).send("User not found");
+            }
+        })
+    }
+]
 // Now using exports.func so the list of below doesnt get too long
 // Route to create the employer's account
 // module.exports = {
