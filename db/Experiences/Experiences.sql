c63aedea48099dfac38b8803442ed269cad21cfb
Patrick Shyong <pshyong@gmail.com>
2020-01-31 15:52:23 -0500

    [AR-5] Fix experiences table

--- db/Experiences/Experiences.sql
+++ db/Experiences/Experiences.sql
@@ -1,7 +1,8 @@
 CREATE table IF NOT EXISTS Experiences (
-    UID uuid REFERENCES jobseeker(uid) PRIMARY KEY ON DELETE CASCADE,
+    UID uuid REFERENCES jobseeker(uid) ON DELETE CASCADE,
     Title VARCHAR(80) not NULL,
     start_date VARCHAR(10) not null,
     end_date VARCHAR(10) not null,
-    description TEXT not NULL
+    description TEXT not NULL,
+    PRIMARY KEY (UID)
 );
