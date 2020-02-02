75230deacb248501b30296a5342cf47c21293323
Daffy Chu <danielchu0710@hotmail.com>
2020-02-01 23:31:39 -0500

    Commit for testing

--- db/Employer/Employer.sql
+++ db/Employer/Employer.sql
@@ -1,8 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE table IF NOT EXISTS Employer (
---     UID uuid DEFAULT uuid_generate_v4 () primary key,
---     uid uuid references users(uid),
     Company_ID uuid REFERENCES company(company_id) ON DELETE CASCADE,
     Successful_Searches int not NULL DEFAULT 0,
     Interview_Req_Sent int not NULL DEFAULT 0,
