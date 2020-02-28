03deb2ef48e02916ccea7b86675c363f63b57d45
Daniel Chu <danielchu0710@hotmail.com>
2020-02-28 15:38:23 -0500

    Update database

--- db/Company/Company.sql
+++ db/Company/Company.sql
@@ -1,9 +1,10 @@
 CREATE table IF NOT EXISTS Company (
-    Company_ID serial primary key,
+    Company_ID serial not NULL,
     Company_Name varchar(70) not NULL,
     Date_Created date not NULL DEFAULT current_date,
     Successful_Hires int not NULL DEFAULT 0,
     Num_Jobs int not NULL DEFAULT 0,
     Num_Employers int not NULL DEFAULT 0,
-    Active_State boolean default true
+    Active_State boolean default true,
+    PRIMARY KEY (Company_ID, Company_Name)  
 );
