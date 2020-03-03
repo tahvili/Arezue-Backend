3b895a2a93b97e6c42b3addd997854e4a826d8dd
Daffy Chu <danielchu0710@hotmail.com>
2020-03-02 23:17:12 -0500

    [AR-42] Add alll required column to jobseeker table

--- db/Jobseeker/Jobseeker.sql
+++ db/Jobseeker/Jobseeker.sql
@@ -7,7 +7,9 @@ CREATE table IF NOT EXISTS Jobseeker (
     Num_Employer_Viewed_Resume int not NULL DEFAULT 0,
     Pending_Interest int not NULL DEFAULT 0,
     Potential_Client boolean not NULL DEFAULT False,
-
+    Acceptance_Wage decimal(10, 2) DEFAULT 0,
+    Goal_Wage decimal(10, 2) DEFAULT 0,
+    Open_relocation boolean not NULL default False, 
     PRIMARY KEY(uid)
 ) INHERITS (Users);
 
