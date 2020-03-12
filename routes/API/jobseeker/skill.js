b9bf4c3b8f3373f75ea19dea5409cb993d4e782c
Jiun Yenn Shyong <shyongji@mail.utoronto.ca>
2020-03-12 01:15:53 -0400

    [AR-69] Stuck on addSkill

--- routes/API/jobseeker/skill.js
+++ routes/API/jobseeker/skill.js
@@ -48,6 +48,7 @@ exports.getSkills = [
     }
 ];
 
+
 exports.addSkill = [
     async function (req, res, next) {
         let uid = validator.escape(req.params.uid);
@@ -62,60 +63,102 @@ exports.addSkill = [
             res.status(400).send("Invalid UUID");
             return;
         }
-        let skillNameQuery = `SELECT id FROM pre_skills WHERE LOWER(skill) = LOWER($1)`;
-        
+    // https://stackoverflow.com/questions/28250680/how-do-i-access-previous-promise-results-in-a-then-chain
+    // https://stackoverflow.com/questions/39693228/building-promise-chain-with-logic-using-postgresql
         var skill_id = 0;
-        Promise.all([pool.query(skillNameQuery, [skill])])
-            .then(result => {
-                var rows = result.filter(r => r.rowCount > 0).map(r => r.rows);
-                if (rows[0]) {
-                    skill_id = rows[0][0].id;
-                    console.log(`if ${skill_id}`);
-                } else {
-                    pool.query(`INSERT INTO pre_skills(skill) values(LOWER($1)) RETURNING id;`, [skill], (error, response) => {
-                        if (error) {
-                            res.status(400).send(`Adding new predefined skill error: ${error}`);
-                            return;
-                        } else {
-                            skill_id = response.rows[0].id;
-                            console.log(response)
-                            console.log(`else ${skill_id}`)
-                        }
-                    })
-                }
-                    let Query = `INSERT INTO skills (uid, skill_id, level, years) VALUES ($1, $2, $3, $4) returning uid`
-                    pool.query(Query, [uid, skill_id, level, years], (error, response) => {
-                        if (error) {
-                            console.log(`final ${skill_id}`)
-                            res.status(400).send(`Adding new jobseeker skill ${error}`);
-                            return;
-                        } else {
-                            res.status(200).send("Added jobseeker skill")
-                        }
-                    })
+        let getSkillId = `SELECT id FROM pre_skills WHERE LOWER(skill) = LOWER($1)`;
+        let addPreSkill = `INSERT INTO pre_skills(skill) values(LOWER($1)) RETURNING id;`;
+        let addSkill = `INSERT INTO skills (uid, skill_id, level, years) VALUES ($1, $2, $3, $4) returning uid`;
+
+        var promise = pool.query(getSkillId, [skill])
+        .then(res => {
+            console.log(res.rows[0])
+            if (res.rows[0]) {
+                
+            }
+        })
+        .catch(e => console.error(e.stack))
+    //    console.log(promise);
+        // var promise2 = promise.then(pool.query(addSkill, [uid, skill_id]) {
+
+        // })
 
 
-            }).catch(e => {
-                res.status(500);
-                res.send(sendError(500, '/jobseeker error ' + e))
-            });
-      
-       
-//         Promise.all([pool.query(Query, [uid, skill_id, level, years])])
+    }];
+
+// exports.addSkill = [
+//     async function (req, res, next) {
+//         let uid = validator.escape(req.params.uid);
+//         let skill = validator.escape(req.body.skill);
+//         let level = validator.escape(req.body.level);
+//         let years = validator.escape(req.body.years);
+//         if (validator.isEmpty(uid) || validator.isEmpty(skill)) {
+//             res.status(400).send("One of the field is empty");
+//             return;
+//         }
+//         if (!validator.isUUID(uid, [4])) {
+//             res.status(400).send("Invalid UUID");
+//             return;
+//         }
+//         let skillNameQuery = `SELECT id FROM pre_skills WHERE LOWER(skill) = LOWER($1)`;
+//         var skill_id = 0;
+
+
+//         Promise.all([pool.query(skillNameQuery, [skill])])
 //             .then(result => {
 //                 var rows = result.filter(r => r.rowCount > 0).map(r => r.rows);
-//                 console.log(rows)
-//                 if (rows[0]) {
-//                     res.status(200).send(rows);
-//                 } else {
-//                     res.status(400).send(`Jobseeker could not be found`);
+//                 try {
+
+
+//                     if (rows[0]) {
+//                         skill_id = rows[0][0].id;
+//                         console.log(`if ${skill_id}`);
+//                     } else {
+//                         pool.query(`INSERT INTO pre_skills(skill) values(LOWER($1)) RETURNING id;`, [skill], (error, response) => {
+//                             if (error) {
+//                                 res.status(400).send(`Adding new predefined skill error: ${error}`);
+//                                 return;
+//                             } else {
+//                                 skill_id = response.rows[0].id;
+//                                 console.log(response)
+//                                 console.log(`else ${skill_id}`)
+//                             }
+//                         })
+//                     }
+//                 } finally {
+//                     let Query = `INSERT INTO skills (uid, skill_id, level, years) VALUES ($1, $2, $3, $4) returning uid`
+//                     pool.query(Query, [uid, skill_id, level, years], (error, response) => {
+//                         if (error) {
+//                             console.log(`final ${skill_id}`)
+//                             res.status(400).send(`Adding new jobseeker skill ${error}`);
+//                             return;
+//                         } else {
+//                             res.status(200).send("Added jobseeker skill")
+//                         }
+//                     })
 //                 }
+
 //             }).catch(e => {
 //                 res.status(500);
 //                 res.send(sendError(500, '/jobseeker error ' + e))
 //             });
-//     }
-    }];
+
+
+        //         Promise.all([pool.query(Query, [uid, skill_id, level, years])])
+        //             .then(result => {
+        //                 var rows = result.filter(r => r.rowCount > 0).map(r => r.rows);
+        //                 console.log(rows)
+        //                 if (rows[0]) {
+        //                     res.status(200).send(rows);
+        //                 } else {
+        //                     res.status(400).send(`Jobseeker could not be found`);
+        //                 }
+        //             }).catch(e => {
+        //                 res.status(500);
+        //                 res.send(sendError(500, '/jobseeker error ' + e))
+        //             });
+        //     }
+    // }];
 
 exports.deleteSkill = [
     async function (req, res, next) {
