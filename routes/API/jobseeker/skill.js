1609723c4961480bf43b42beca7499915266c291
Jiun Yenn Shyong <shyongji@mail.utoronto.ca>
2020-03-13 00:55:13 -0400

    [AR-69] Update getSkills and addSkills

--- routes/API/jobseeker/skill.js
+++ routes/API/jobseeker/skill.js
@@ -31,19 +31,41 @@ exports.getSkills = [
             return;
         }
         let Query = `SELECT * FROM skills where uid = $1`;
-        Promise.all([pool.query(Query, [uid])])
-            .then(result => {
-                var rows = result.filter(r => r.rowCount > 0).map(r => r.rows)
-
-                if (rows[0]) {
-                    res.status(200).send(rows[0])
+        let query2 = `SELECT Skill from Pre_Skills where id = $1`;
+        var skills = [];
+        await Promise.all([pool.query(Query, [uid])])
+            .then(async result => {
+                var row1 = result.filter(r => r.rowCount > 0).map(r => r.rows)
+                var skills = [];
+                for (i = 0; i < row1[0].length; i++) {
+                    var years = row1[0][i].years;
+                    var level = row1[0][i].level;
+                    await Promise.all([pool.query(query2, [row1[0][i].skill_id])])
+                        .then(result2 => {
+                            var row2 = result2.filter(r => r.rowCount > 0).map(r => r.rows);
+                            var ele = {};
+                            ele.skill = row2[0][0].skill;
+                            ele.years = years;
+                            ele.level = level;
+                            skills.push(ele);
+                        })
+                        .catch(e => {
+                            res.status(500);
+                            res.send(sendError(500, '/jobseeker ' + e))
+                            return;
+                        });
+                }
+                if (skills) {
+                    res.status(200).send(skills)
+                    return;
                 } else {
                     res.status(400).send(`Jobseeker could not be found`);
                 }
             })
+
             .catch(e => {
                 res.status(500);
-                res.send(sendError(500, '/jobseeker error ' + e))
+                res.send(sendError(500, '/jobseeker ' + e))
             });
     }
 ];
@@ -63,22 +85,14 @@ exports.addSkill = [
             res.status(400).send("Invalid UUID");
             return;
         }
-        // https://stackoverflow.com/questions/28250680/how-do-i-access-previous-promise-results-in-a-then-chain
-        // https://stackoverflow.com/questions/39693228/building-promise-chain-with-logic-using-postgresql
-        // https://stackoverflow.com/questions/33257412/how-to-handle-the-if-else-in-promise-then
-        // https://stackoverflow.com/questions/40804351/how-do-i-chain-multiple-conditional-promises
         let getSkillId = `SELECT id FROM pre_skills WHERE LOWER(skill) = LOWER($1)`;
         let addPreSkill = `INSERT INTO pre_skills(skill) values(LOWER($1)) RETURNING id;`;
         let addSkill = `INSERT INTO skills(uid, skill_id, level, years) VALUES($1, $2, $3, $4) returning uid`;
-
         await Promise.all([pool.query(getSkillId, [skill])])
             .then(result => {
-                // console.log(result);
-                // console.log(result[0].rowCount);
                 // skill is in pre_skills table. Proceed to add jobseeker's skill
                 if (result[0].rowCount > 0) {
                     row = result.map(r => r.rows);
-                    // console.log(row)
                     Promise.all([pool.query(addSkill, [uid, row[0][0].id, level, years])])
                         .then(result2 => {
                             row = result2.map(r => r.rows);
@@ -90,12 +104,11 @@ exports.addSkill = [
                             res.send(sendError(500, '/jobseeker1 ' + e))
                             return;
                         });
-                // skill is NOT in pre_skills table. Need to add skill to pre_skill first
+                    // skill is NOT in pre_skills table. Need to add skill to pre_skill first
                 } else {
                     Promise.all([pool.query(addPreSkill, [skill])])
                         .then(result3 => {
                             row = result3.map(r => r.rows)
-                            // console.log(`preskill: ${row[0][0].id}`);
                             // Now we can add to skills
                             Promise.all([pool.query(addSkill, [uid, row[0][0].id, level, years])])
                                 .then(result4 => {
@@ -105,20 +118,20 @@ exports.addSkill = [
                                 })
                                 .catch(e => {
                                     res.status(500);
-                                    res.send(sendError(500, '/jobseeker2 ' + e))
+                                    res.send(sendError(500, '/jobseeker ' + e))
                                     return;
                                 });
                         })
                         .catch(e => {
                             res.status(500);
-                            res.send(sendError(500, '/jobseeker3 ' + e))
+                            res.send(sendError(500, '/jobseeker ' + e))
                             return;
                         });
                 }
             })
             .catch(e => {
                 res.status(500);
-                res.send(sendError(500, '/jobseeker4 ' + e))
+                res.send(sendError(500, '/jobseeker ' + e))
                 return;
             });
     }];
