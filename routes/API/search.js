b31a3060b2982751e0065b5485fc83abaef078b3
Jiun Yenn Shyong (Patrick) <p.shyong@mail.utoronto.ca>
2020-04-17 14:43:13 -0400

    [AR-90] Redefine routes

--- routes/API/search.js
+++ routes/API/search.js
@@ -39,4 +39,44 @@ router.route('/')
      */
     .post(search.searchCandidates);
 
+    router.route('/skills')
+    /**
+     *  @swagger
+     * 
+     *  /search/skill/search?q={q}&?limit={limit}:
+     *      get:
+     *          description: Search the predefiled skills table based on query
+     *          tags:
+     *              - Jobseeker, skills
+     *          produces: 
+     *              - application/json
+     *          parameters:
+     *              - name: uid
+     *                description: The UID of the jobseeker
+     *                in: path
+     *                required: true
+     *                type: string
+     *              - name: q
+     *                description: The requested query
+     *                in: query
+     *                requried: true
+     *                type: string
+     *              - name: limit
+     *                description: The requested query
+     *                in: query
+     *                requried: true
+     *                type: integer
+     * 
+     *          responses:
+     *              200:
+     *                  description: Successfully returned all matching string
+     *              400:
+     *                  description: Field incorrect
+     *              500:
+     *                  description: Internal server error
+     * 
+     * 
+     */
+    .get(skill.searchSkill)
+
     module.exports = router;
